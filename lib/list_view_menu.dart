e781b20a8c6df1f2beae1ce580ff617cb92be6d9
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-29 11:37:28 +0700

    update

--- lib/list_view_menu.dart
+++ lib/list_view_menu.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData? Icon;
+  const ListMenu({this.title, this.subtitle, this.Icon});
+}
+
+List<ListMenu> menus = [
+  const ListMenu(title: 'MENU-1', subtitle: 'SUB-1', Icon: Icons.person),
+];
+
 class ListViewMenuPage extends StatelessWidget {
   const ListViewMenuPage({Key? key}) : super(key: key);
 
@@ -12,20 +23,19 @@ class ListViewMenuPage extends StatelessWidget {
       ),
       body: Container(
         child: ListView.separated(
-          itemCount: 4,
-          separatorBuilder: (_,index){
+          itemCount: 5,
+          separatorBuilder: (_, index) {
             return Divider(color: Colors.red);
           },
-          itemBuilder: (context,index){
+          itemBuilder: (context, index) {
             return ListTile(
-              title: Text('Menu-1'),
-              subtitle: Text('SUB-1'),
-              leading: CircleAvatar(child: Icon(Icons.person)),
-              trailing: Icon(Icons.arrow_right),
-              onTap: () {
-                debugPrint('MENU-1');
-              }
-            );
+                title: Text('Menu-1'),
+                subtitle: Text('SUB-1'),
+                leading: CircleAvatar(child: Icon(Icons.person)),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {
+                  debugPrint('MENU-1');
+                });
           },
         ),
       ),
